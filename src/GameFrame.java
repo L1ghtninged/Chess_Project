7e6f4f84e22c4f086c969646f18cbdb0d746e68e
L1ghtninged <133992387+L1ghtninged@users.noreply.github.com>
2024-06-01 14:45:53 +0200

    Jar file added

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -25,6 +25,27 @@ public class GameFrame extends JFrame{
         this.setTitle("Chess");
 
     }
+    public GameFrame(){
+        ChessGame game = new ChessGame(Board.startPosition);
+        board = new GamePanel(game, panel);
+        panel = new SettingsPanel(game, board);
+        board.settingsPanel = panel;
+
+        this.add(panel);
+        this.add(board);
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setSize(614, 735);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setTitle("Chess");
+
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(GameFrame::new);
+    }
 
 
 }
